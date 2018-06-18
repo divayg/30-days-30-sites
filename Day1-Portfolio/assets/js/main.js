d0dd574010f43f91a4daeb657f43246b1bf184cf
Divay Garg <divay.garg.work@gmail.com>
2018-06-19 02:54:42 +0530

    added content

--- Day1-Portfolio/assets/js/main.js
+++ Day1-Portfolio/assets/js/main.js
@@ -9,7 +9,7 @@ let navShift=navWidth-iconsWidth;
 
 navbar.style.left=`-${Math.floor(navShift)-10}px`;
 navbar.addEventListener("mouseover",()=>(navbar.style.left="0px"));
-navbar.addEventListener("mouseout",()=>(navbar.style.left=`-${Math.floor(navShift)-10}px`));
+navbar.addEventListener("mouseout",()=>(navbar.style.left=`-${Math.floor(navShift)}px`));
 
 let menuBar=document.querySelector(".menu-bar");
 menuBar.addEventListener("click",()=>{
@@ -47,11 +47,11 @@ $(".navbar li a").click(function(e){
     },1000);
 
     $('.navbar li a.active').removeClass('active');
-        $(this).addClass('active');
+    $(this).addClass('active');
         e.preventDefault();
 });
 
-/*** Scroll-spy Function */
+/*** Scroll-spy Function ***/
 $(window).scroll(function() {
     var x = document.documentElement.scrollTop || document.body.scrollTop;
     $("section.scroll").each(function(index) {
@@ -59,7 +59,75 @@ $(window).scroll(function() {
       if (x >= $(this).offset().top && x < $(this).offset().top + $(this).height()) {
         $(`[data-value='${z}']`).addClass("active");
       } else {
-        $(`[data-value='${z}']`).removeClass("active")
+        $(`[data-value='${z}']`).removeClass("active");
       }
     })
   })
+
+  var str=[" Divay Garg "," a Front End Web Developer"," a web enthusiast !"];
+  var pos=0,a=0;
+  var innerText="";
+    function displayText(){
+      if(pos>2) pos=0;    
+          
+          if(a<str[pos].length)
+          {
+            innerText+=str[pos].charAt(a);
+            $(".text-change").html(innerText);
+            a++;
+          }
+      else {
+        a=0;
+        pos++;
+        innerText="";
+      }
+     }
+     setTimeout(setInterval(displayText,150),2000);
+
+     $(document).ready(function(){
+
+        (function($) {
+        
+          $.fn.visible = function(partial) {
+            
+              var $t            = $(this),
+                  $w            = $(window),
+                  viewTop       = $w.scrollTop(),
+                  viewBottom    = viewTop + $w.height(),
+                  _top          = $t.offset().top,
+                  _bottom       = _top + $t.height(),
+                  compareTop    = partial === true ? _bottom : _top,
+                  compareBottom = partial === true ? _top : _bottom;
+                
+            return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+        
+          };
+            
+        })(jQuery);
+        
+        var win = $(window);
+        var cards=$(".service");
+        var outerBar= $(".outer-bar");
+        var percentage=$(".percentage")
+
+win.scroll(function(event) {
+  
+  cards.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("slideInRight"); 
+    } 
+
+  });
+
+  outerBar.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("outer-bar-animate"); 
+      percentage.css("display","block");
+    } 
+
+  });
+  
+});
+    });
